40bb457de4614c6ade9a6bcd98bf105cc357ba13
oygull <oygulkhom3008@gmail.com>
2022-03-20 16:58:26 +0500

    changes

--- js/main.js
+++ js/main.js
@@ -8,24 +8,28 @@ let elSorting = document.getElementById('sorting')
 let newArr = [];
 let contacts = [
   {
+    id:1,
     firstName: 'Nozigul',
     lastName: 'Ibrokhimova',
     telNum: '+998995643245',
     categoryItem: 'relative'
   },
    {
+    id:2,
     firstName: 'Laylo',
     lastName: 'Kasimaliyeva',
     telNum: '+998995643245',
     categoryItem: 'friend'
   },
    {
+    id:3,
     firstName: 'Jaloliddin',
     lastName: 'Boboyev',
     telNum: '+998995643245',
     categoryItem: 'relative'
   },
   {
+    id:4,
     firstName: 'Khadichabegim',
     lastName: 'Rakhimova',
     telNum: '+998990012838',
@@ -57,16 +61,18 @@ function showItems(e){
  e.forEach(el => {
    let li = document.createElement('li');
    li.innerHTML = `
-     <div class="">
+     <div >
                 <p class="first-name">${el.firstName}</p>
                 <p class="last-name">${el.lastName}</p>
               </div>
               <div>
                 <p class="category">${el.categoryItem}</p>
                 <a href="telto:${el.telNum}" class="tel-num"><i class='bx bxs-phone-call'></i></a>
+                <button onclick="removeItem(${el.id})" class="delete-btn"><i class='bx bxs-x-circle'></i></button>
               </div>
    `
-   li.classList="contact-item";
+   
+  li.classList = `contact-item`
    firstList.appendChild(li);
  });
 }
@@ -74,20 +80,41 @@ function showItems(e){
 
 
 elSorting.addEventListener('click', sortingItems);
+let newArr2 = contacts.concat(newArr);
 
 function sortingItems(e){
+  firstList.innerHTML='';
 
 
- if(e.target.value='friend'){
-    firstList.innerHTML='';
-    let removedArr = []
-    contacts.forEach((el)=>{
-     if(el.categoryItem=='friend'){
-      removedArr.push(el)
-     }
-    })
-    showItems(removedArr);
+  switch(e.target.value){
+    case "all": 
+      showItems(newArr2);
+    break;
+    case "relatives": 
+    let a = newArr2.filter((el)=>{ 
+     if(el.categoryItem=='relative') return true; 
+      })
+      showItems(a);
+     break;
+     case "friends": 
+     let b = newArr2.filter((el)=>{ 
+      if(el.categoryItem=='friend') return true; 
+       })
+       showItems(b);
+      break;
   }
-
 }
 
+function removeItem(e){
+  let removedArr=[];
+  if(confirm("Dou you want to delete?") == true){
+    firstList.innerHTML='';
+    newArr2.forEach((el)=>{
+      if(el.id!==e){
+          removedArr.push(el)
+      }
+    })
+  }
+  newArr2=removedArr
+  showItems(newArr2);
+}
